50c1245116d9b549701a73d2f119a473480172d3
FernandaRoblesMts <fernandarrobles2002@gmail.com>
2023-02-01 14:04:01 -0600

    Ejercicio7

---
+++ Practicas/P02/indexphp7.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"&gt;
+<html>
+<head>
+    <title>Práctica 2</title>
+</head>
+<body>
+    <h2>Inciso 7</h2>
+    <?php
+    
+    
+echo "Apache version: " . $_SERVER['SERVER_SOFTWARE'] . PHP_EOL;
+echo "<br>PHP version: " . PHP_VERSION . PHP_EOL;
+echo "<br>OS: " . PHP_OS . PHP_EOL;
+
+
+
+
+    
+    ?>
+</body>
+</html>
