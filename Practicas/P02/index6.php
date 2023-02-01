9319774fc4ac23f8415b67b221ae3adeadd4656e
FernandaRoblesMts <fernandarrobles2002@gmail.com>
2023-02-01 14:12:00 -0600

    Ejercicio6.2

--- Practicas/P02/index6.php
+++ Practicas/P02/index6.php
@@ -4,27 +4,40 @@
     <title>Práctica 2</title>
 </head>
 <body>
-    <h2>Inciso 6</h2>
+    <h2>Inciso 2</h2>
+    <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
+    <p>$_myvar, $_7var, myvar, $myvar, $var7, $_element1, $house*5</p>
     <?php
-    
-    
-    $a = "0";
-    $b = "TRUE";
+    $a = '0';
+    $b = 'TRUE';
     $c = FALSE;
     $d = ($a OR $b);
     $e = ($a AND $c);
     $f = ($a XOR $b);
-    
-    echo "Valor de \$a: " . var_export((bool)$a, true) . PHP_EOL;
-    echo "<br>Valor de \$b: " . var_export((bool)$b, true) . PHP_EOL;
-    echo "<br>Valor de \$c: " . var_export($c, true) . PHP_EOL;
-    echo "<br>Valor de \$d: " . var_export($d, true) . PHP_EOL;
-    echo "<br>Valor de \$e: " . var_export($e, true) . PHP_EOL;
-    echo "<br>Valor de \$f: " . var_export($f, true) . PHP_EOL;
-
-
-
-    
+    $g = 'El valor de a es';
+    $valorb = 'El valor de b es ';
+    $valorc = 'El valor de c es ';
+    $valord = 'El valor de d es ';
+    $valore = 'El valor de e es ';
+    $valorf = 'El valor de f es ';
+    $espacio = '<br>';
+    echo $g;
+    var_dump($a);
+    echo $espacio;
+    echo $valorb;
+    var_dump($b);
+    echo $espacio;
+    echo $valorc;
+    var_dump($c);
+    echo $espacio;
+    echo $valord;
+    var_dump($d);
+    echo $espacio;
+    echo $valore;
+    var_dump($e);
+    echo $espacio;
+    echo $valorf;
+    var_dump($f);
     ?>
-</body>
-</html>
+    </body>
+    </html>
