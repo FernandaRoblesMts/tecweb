ef2b2c79b5d1c698079963c3e9f37048ee3cb3fe
FernandaRoblesMts <fernandarrobles2002@gmail.com>
2023-02-01 14:02:21 -0600

    Ejercicio2

---
+++ Practicas/P02/index2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"&gt;
+<html>
+<head>
+    <title>Práctica 2</title>
+</head>
+<body>
+    <h2>Inciso 2</h2>
+    <?php
+    $a = "manejadorsql";
+    echo "$a<br>";
+
+    $b = "MySQL";
+    echo "$b<br>";
+
+    $c = "&$a";
+    echo "$c<br>";
+    
+    ?>
+</body>
+</html>
