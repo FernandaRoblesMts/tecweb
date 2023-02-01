7e79f3c0a6ef7f25b79d64d4891447ed5990643d
FernandaRoblesMts <fernandarrobles2002@gmail.com>
2023-02-01 16:49:54 -0600

    Practica6.3

--- Practicas/P02/index6.php
+++ Practicas/P02/index6.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"&gt;
 <html>
 <head>
-    <title>Práctica 2</title>
+    <title>Práctica 6</title>
 </head>
 <body>
-    <h2>Inciso 2</h2>
-    <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
-    <p>$_myvar, $_7var, myvar, $myvar, $var7, $_element1, $house*5</p>
+    <p>Practica 6</p>
     <?php
     $a = '0';
     $b = 'TRUE';
@@ -14,12 +12,12 @@
     $d = ($a OR $b);
     $e = ($a AND $c);
     $f = ($a XOR $b);
-    $g = 'El valor de a es';
-    $valorb = 'El valor de b es ';
-    $valorc = 'El valor de c es ';
-    $valord = 'El valor de d es ';
-    $valore = 'El valor de e es ';
-    $valorf = 'El valor de f es ';
+    $g = 'a = ';
+    $valorb = 'b = ';
+    $valorc = 'c = ';
+    $valord = 'd = ';
+    $valore = 'e = ';
+    $valorf = 'f = ';
     $espacio = '<br>';
     echo $g;
     var_dump($a);
