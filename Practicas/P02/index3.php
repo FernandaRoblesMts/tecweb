80ee79ff7a72672792a050a8bd76ba54525a9aa1
FernandaRoblesMts <fernandarrobles2002@gmail.com>
2023-02-01 14:02:46 -0600

    Ejercicio3

---
+++ Practicas/P02/index3.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"&gt;
+<html>
+<head>
+    <title>Práctica 2</title>
+</head>
+<body>
+    <h2>Inciso 3</h2>
+    <?php
+    
+    $a = "PHP5";
+    echo "$a<br>";
+    //$z []="&$a";
+    //echo "$z<br>";
+    $b = "5a version de php";
+    echo "$b<br>";
+    //$c = $b*10;
+    //echo "$c<br>";
+    $a.=$b;
+    $b*=$c;
+    $z[0]="MYSQL";
+    echo "$z[0]<br>";
+
+
+
+
+    
+    ?>
+</body>
+</html>
