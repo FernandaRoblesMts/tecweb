635dc37bfe2e1bd32cc4208da003701fe38cc544
FernandaRoblesMts <fernandarrobles2002@gmail.com>
2023-02-23 20:02:05 -0600

    Productoxhtml

--- Practicas/P05/get_productos_xhtml.php
+++ Practicas/P05/get_productos_xhtml.php
@@ -1,47 +1,78 @@
-<?php
-header("Content-Type: application/xhtml+xml; charset=utf-8");
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+	<?php
+	if(isset($_GET['tope']))
+		$tope = $_GET['tope'];
+if (!empty($tope))
+{
+	/** SE CREA EL OBJETO DE CONEXION */
+	@$link = new mysqli('localhost', 'root', '123456', 'marketzone');	
 
-if (isset($_GET['tope'])) {
-    $tope = $_GET['tope'];
-} else {
-    die('Parámetro "tope" no detectado...');
-}
+	/** comprobar la conexión */
+	if ($link->connect_errno) 
+	{
+		die('Falló la conexión: '.$link->connect_error.'<br/>');
+		    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+	}
 
-if (!empty($tope)) {
-    // SE CREA EL OBJETO DE CONEXIÓN
-    @$link = new mysqli('localhost', 'root', '123456', 'marketzone');
-    // NOTA: con @ se suprime el Warning para gestionar el error por medio de código
+	/** Crear una tabla que no devuelve un conjunto de resultados */
+	if ( $result = $link->query("SELECT * FROM productos WHERE unidades <= '{$tope}'") ) 
+	{
+		$rows = $result->fetch_all(MYSQLI_ASSOC);
+		/** útil para liberar memoria asociada a un resultado con demasiada información */
+		$result->free();
+	}
 
-    // COMPROBAR LA CONEXIÓN
-    if ($link->connect_errno) {
-        die('Falló la conexión: '.$link->connect_error.'<br/>');
-    }
-
-    // OBTENER LOS PRODUCTOS CON UNIDADES MENORES O IGUALES A $tope
-    $query = "SELECT * FROM productos WHERE unidades <= $tope";
-    $result = $link->query($query);
-
-    // CREAR UN DOCUMENTO XHTML CON LOS DATOS OBTENIDOS
-    echo '<?xml version="1.0" encoding="UTF-8"?>';
-    echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
-    echo '<html xmlns="http://www.w3.org/1999/xhtml">';
-    echo '<head><title>Productos con unidades menores o iguales a '.$tope.'</title></head>';
-    echo '<body>';
-    echo '<table>';
-    echo '<thead><tr><th>ID</th><th>Nombre</th><th>Marca</th><th>Modelo</th><th>Detalles</th><th>Precio</th><th>Unidades</th></tr></thead>';
-    echo '<tbody>';
-    while ($row = $result->fetch_assoc()) {
-        echo '<tr><td>'.$row['id'].'</td><td>'.$row['nombre'].'</td><td>'.$row['marca'].'</td><td>'.$row['modelo'].'</td><td>'.$row['detalles'].'</td><td>'.$row['precio'].'</td><td>'.$row['unidades'].'</td></tr>';
-    }
-    echo '</tbody>';
-    echo '</table>';
-    echo '</body>';
-    echo '</html>';
-
-    // LIBERAR LA MEMORIA ASOCIADA AL RESULTADO
-    $result->free();
-
-    // CERRAR LA CONEXIÓN
-    $link->close();
+	$link->close();
 }
 ?>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Productos</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+	<h3>PRODUCTOS</h3>
+
+	<br/>
+	
+	<?php if( isset($rows) ) : ?>
+
+		<table class="table">
+			<thead class="thead-dark">
+				<tr>
+				<th scope="col">#</th>
+				<th scope="col">Nombre</th>
+				<th scope="col">Marca</th>
+				<th scope="col">Modelo</th>
+				<th scope="col">Precio</th>
+				<th scope="col">Unidades</th>
+				<th scope="col">Detalles</th>
+				<th scope="col">Imagen</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach($rows as $row) : ?>
+				<tr>
+					<th scope="row"><?= $row['id'] ?></th>
+					<td><?= $row['nombre'] ?></td>
+					<td><?= $row['marca'] ?></td>
+					<td><?= $row['modelo'] ?></td>
+					<td><?= $row['precio'] ?></td>
+					<td><?= $row['unidades'] ?></td>
+					<td><?= utf8_encode($row['detalles']) ?></td>
+					<td><img src=<?= $row['imagen'] ?> ></td>
+				</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
+
+	<?php elseif(!empty($tope)) : ?>
+
+		 <script>
+            alert('No hay productos con una cantidad de unidades menor o igual al tope especificado');
+         </script>
+
+	<?php endif; ?>
+</body>
+</html>
